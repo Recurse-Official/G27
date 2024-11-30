fe14f893a3cea88042670833882b8355f2ad9d90
krishnamanojpvr <pvrkmsbunny@gmail.com>
2024-12-01 01:46:06 +0530

    config

--- docwizNodePackage/createConfig.js
+++ docwizNodePackage/createConfig.js
@@ -2,20 +2,13 @@
 
 const fs = require('fs');
 const path = require('path');
-
-// Default configuration
 const config = {
     name: "your-project-name",
     "input_path": "./input",
     "output_path": "./output"
 };
-
-// Determine the user's project root
 const userProjectRoot = process.cwd(); 
-
-
 const configPath = path.join(userProjectRoot, 'docwiz.config.js');
-
 fs.writeFile(configPath, `module.exports = ${JSON.stringify(config, null, 2)};`, (err) => {
     if (err) {
         console.error('Error writing config file:', err);
@@ -23,9 +16,7 @@ fs.writeFile(configPath, `module.exports = ${JSON.stringify(config, null, 2)};`,
         console.log('Config file created successfully at', configPath);
     }
 });
-
 config["name-of-file"] = "docwiz.config.js";
-
 fs.writeFile(configPath, `module.exports = ${JSON.stringify(config, null, 2)};`, (err) => {
     if (err) {
         console.error('Error writing updated config file:', err);
